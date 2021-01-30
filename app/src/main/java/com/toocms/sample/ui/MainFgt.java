d78387da4b7198a4c74dcdea9a3d04a1c919424e
toocms <98562@qq.com>
2021-01-30 09:36:58 +0800

    同步RecyclerView设置分割线颜色以及宽度的代码

--- app/src/main/java/com/toocms/sample/ui/MainFgt.java
+++ app/src/main/java/com/toocms/sample/ui/MainFgt.java
@@ -1,5 +1,8 @@
 package com.toocms.sample.ui;
 
+import android.graphics.Color;
+
+import com.blankj.utilcode.util.LogUtils;
 import com.toocms.sample.R;
 import com.toocms.sample.ui.expand.ExpandFgt;
 import com.toocms.sample.ui.tool.ToolFgt;
@@ -17,6 +20,7 @@ public class MainFgt extends BaseBottomTabSegmentFragment {
 
     @Override
     protected TabSegmentItem[] getTabSegmentItems() {
+        LogUtils.e(Color.parseColor("#F6F4F4"));
         return new TabSegmentItem[]{
                 new TabSegmentItem(R.drawable.ic_tab_tool_normal, R.drawable.ic_tab_tool_selected, "工具", ToolFgt.class),
                 new TabSegmentItem(R.drawable.ic_tab_widget_normal, R.drawable.ic_tab_widget_selected, "控件", WidgetFgt.class),
