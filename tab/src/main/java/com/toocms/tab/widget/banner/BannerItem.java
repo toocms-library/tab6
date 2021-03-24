388c3f581ae58a6a64b3f69bbc251614f31aa6b6
toocms <98562@qq.com>
2021-03-24 21:06:27 +0800

    优化BannerItem类，扩充跳转规则以及扩展属性

--- tab/src/main/java/com/toocms/tab/widget/banner/BannerItem.java
+++ tab/src/main/java/com/toocms/tab/widget/banner/BannerItem.java
@@ -8,8 +8,22 @@ package com.toocms.tab.widget.banner;
  */
 public class BannerItem {
 
-    public String imgUrl;
     public String title;
+    public String imgUrl;
+    public String target_rule;
+    public String param;
+    public String expand1;
+    public String expand2;
+    public String expand3;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BannerItem setTitle(String title) {
+        this.title = title;
+        return this;
+    }
 
     public String getImgUrl() {
         return imgUrl;
@@ -20,12 +34,43 @@ public class BannerItem {
         return this;
     }
 
-    public String getTitle() {
-        return title;
+    public String getTarget_rule() {
+        return target_rule;
     }
 
-    public BannerItem setTitle(String title) {
-        this.title = title;
-        return this;
+    public void setTarget_rule(String target_rule) {
+        this.target_rule = target_rule;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+
+    public String getExpand1() {
+        return expand1;
+    }
+
+    public void setExpand1(String expand1) {
+        this.expand1 = expand1;
+    }
+
+    public String getExpand2() {
+        return expand2;
+    }
+
+    public void setExpand2(String expand2) {
+        this.expand2 = expand2;
+    }
+
+    public String getExpand3() {
+        return expand3;
+    }
+
+    public void setExpand3(String expand3) {
+        this.expand3 = expand3;
     }
 }
