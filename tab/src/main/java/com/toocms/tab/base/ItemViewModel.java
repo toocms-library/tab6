66c7f5092a7ef665f7b0c25b63c296377fd51ffc
toocms <98562@qq.com>
2021-04-01 11:01:02 +0800

    将ItemViewModel中的viewModel属性设置为public

--- tab/src/main/java/com/toocms/tab/base/ItemViewModel.java
+++ tab/src/main/java/com/toocms/tab/base/ItemViewModel.java
@@ -8,7 +8,7 @@ import androidx.annotation.NonNull;
  */
 public class ItemViewModel<VM extends BaseViewModel> {
 
-    protected VM viewModel;
+    public VM viewModel;
 
     public ItemViewModel(@NonNull VM viewModel) {
         this.viewModel = viewModel;
