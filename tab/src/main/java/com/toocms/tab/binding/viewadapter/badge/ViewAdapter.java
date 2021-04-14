052363b47c837b6eadc2f44f9f4f494b18acb634
toocms <98562@qq.com>
2021-04-14 19:00:22 +0800

    新增Badge的ViewAdapter属性offsetX、offsetY，单位dp，用于设置X、Y轴的偏移

--- tab/src/main/java/com/toocms/tab/binding/viewadapter/badge/ViewAdapter.java
+++ tab/src/main/java/com/toocms/tab/binding/viewadapter/badge/ViewAdapter.java
@@ -12,40 +12,46 @@ import com.toocms.tab.widget.badge.BadgeView;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * 好像不太好用，暂时先保留
+ * 角标
  * <p>
  * Author：Zero
  * Date：2020/11/13 12:07
  */
 public class ViewAdapter {
 
-    @BindingAdapter(value = {"badgeNum", "textColor", "textSize", "gravity", "backgroundColor"}, requireAll = false)
+    @BindingAdapter(value = {"badgeNum", "textColor", "textSize", "gravity", "backgroundColor", "offsetX", "offsetY"}, requireAll = false)
     public static void bindBadge(@NotNull View targetView,
                                  int badgeNum,
                                  @ColorInt int textColor,
                                  int textSize,
                                  int gravity,
-                                 int backgroundColor) {
+                                 int backgroundColor,
+                                 float offsetX,
+                                 float offsetY) {
         Badge badge = new BadgeView(targetView.getContext()).bindTarget(targetView);
         badge.setBadgeNumber(badgeNum);
         badge.setBadgeTextColor(textColor == 0 ? BadgeView.DEFAULT_COLOR_BADGE_TEXT : textColor);
         badge.setBadgeTextSize(textSize == 0 ? BadgeView.DEFAULT_TEXT_SIZE : textSize, true);
         badge.setBadgeGravity(gravity == 0 ? Gravity.END | Gravity.TOP : gravity);
         badge.setBadgeBackgroundColor(backgroundColor == 0 ? BadgeView.DEFAULT_COLOR_BACKGROUND : backgroundColor);
+        badge.setGravityOffset(offsetX, offsetY, true);
     }
 
-    @BindingAdapter(value = {"badgeText", "textColor", "textSize", "gravity", "backgroundColor"}, requireAll = false)
+    @BindingAdapter(value = {"badgeText", "textColor", "textSize", "gravity", "backgroundColor", "offsetX", "offsetY"}, requireAll = false)
     public static void bindBadge(@NotNull View targetView,
                                  String badgeText,
                                  @ColorInt int textColor,
                                  int textSize,
                                  int gravity,
-                                 int backgroundColor) {
+                                 int backgroundColor,
+                                 float offsetX,
+                                 float offsetY) {
         Badge badge = new BadgeView(targetView.getContext()).bindTarget(targetView);
         badge.setBadgeText(badgeText);
         badge.setBadgeTextColor(textColor == 0 ? BadgeView.DEFAULT_COLOR_BADGE_TEXT : textColor);
         badge.setBadgeTextSize(textSize == 0 ? BadgeView.DEFAULT_TEXT_SIZE : textSize, true);
         badge.setBadgeGravity(gravity == 0 ? Gravity.END | Gravity.TOP : gravity);
         badge.setBadgeBackgroundColor(backgroundColor == 0 ? BadgeView.DEFAULT_COLOR_BACKGROUND : backgroundColor);
+        badge.setGravityOffset(offsetX, offsetY, true);
     }
 }
