9f11c91da0ec6673875c426d5d947385f01be3f7
toocms <98562@qq.com>
2021-05-07 21:09:53 +0800

    将getSkipClass（设置跳转类）回调改为onJumpClick（点击按钮监听）回调

--- tab-expand/src/main/java/com/toocms/tab/expand/guide/BaseGuideActivity.java
+++ tab-expand/src/main/java/com/toocms/tab/expand/guide/BaseGuideActivity.java
@@ -1,12 +1,10 @@
 package com.toocms.tab.expand.guide;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.annotation.DrawableRes;
 import androidx.viewpager.widget.ViewPager;
 
 import com.blankj.utilcode.util.ArrayUtils;
@@ -14,7 +12,6 @@ import com.toocms.tab.base.BaseActivity;
 import com.toocms.tab.expand.R;
 import com.toocms.tab.widget.banner.SimpleGuideBanner;
 import com.toocms.tab.widget.banner.anim.select.ZoomInEnter;
-import com.toocms.tab.widget.banner.transform.DepthTransformer;
 
 import java.util.List;
 
@@ -22,7 +19,7 @@ import java.util.List;
  * Author：Zero
  * Date：2020/11/4 11:46
  */
-public abstract class BaseGuideActivity extends BaseActivity {
+public abstract class BaseGuideActivity extends BaseActivity implements SimpleGuideBanner.OnJumpClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +27,8 @@ public abstract class BaseGuideActivity extends BaseActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.base_layout_guide);
-        initGuideView(getGuideResourceList(), getSkipClass());
+        initGuideView(ArrayUtils.asList(getGuideResourceList()), null);
         onActivityCreated();
     }
 
@@ -50,30 +46,12 @@ public abstract class BaseGuideActivity extends BaseActivity {
     protected abstract Object[] getGuideResourceList();
 
     /**
-     * 获取跳转activity的类
-     *
-     * @return
-     */
-    protected abstract Class<? extends Activity> getSkipClass();
-
-    /**
-     * 初始化引导页
-     *
-     * @param guidesResIdList 引导图片
-     * @param cls             点击后跳转的Activity类
-     */
-    public void initGuideView(Object[] guidesResIdList, final Class<?> cls) {
-        initGuideView(ArrayUtils.asList(guidesResIdList), null, cls);
-    }
-
-    /**
      * 初始化引导页动画
      *
      * @param guidesResIdList  引导图片
      * @param transformerClass 引导图片切换的效果
-     * @param cls              点击后跳转的Activity类
      */
-    public void initGuideView(List<Object> guidesResIdList, Class<? extends ViewPager.PageTransformer> transformerClass, final Class<?> cls) {
+    public void initGuideView(List<Object> guidesResIdList, Class<? extends ViewPager.PageTransformer> transformerClass) {
         SimpleGuideBanner sgb = findViewById(R.id.guide);
 
         sgb.setIndicatorWidth(6).setIndicatorHeight(6).setIndicatorGap(12)
@@ -83,31 +61,6 @@ public abstract class BaseGuideActivity extends BaseActivity {
                 .barPadding(0, 10, 0, 10)
                 .setSource(guidesResIdList).startScroll();
 
-        sgb.setOnJumpClickListener(new SimpleGuideBanner.OnJumpClickListener() {
-            @Override
-            public void onJumpClick() {
-                startActivity(new Intent(BaseGuideActivity.this, cls));
-                finish();
-            }
-        });
-    }
-
-    /**
-     * 初始化引导页动画
-     *
-     * @param guidesResIdList  引导图片
-     * @param transformerClass 引导图片切换的效果
-     */
-    public void initGuideView(List<Object> guidesResIdList, Class<? extends ViewPager.PageTransformer> transformerClass, SimpleGuideBanner.OnJumpClickListener onJumpClickListener) {
-        SimpleGuideBanner sgb = findViewById(R.id.guide);
-
-        sgb.setIndicatorWidth(6).setIndicatorHeight(6).setIndicatorGap(12)
-                .setIndicatorCornerRadius(3.5f)
-                .setSelectAnimClass(ZoomInEnter.class)
-                .setTransformerClass(transformerClass)
-                .barPadding(0, 10, 0, 10)
-                .setSource(guidesResIdList).startScroll();
-
-        sgb.setOnJumpClickListener(onJumpClickListener);
+        sgb.setOnJumpClickListener(this);
     }
 }
